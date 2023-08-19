cf6751d3267793d8680261d9fa296390f5005283
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-19 16:51:53 +0900

    feat: 등급 상수 enum 정의 (#1)

---
+++ src/main/java/fc5/toy3/board/domain/grade/type/GradeType.java
@@ -0,0 +1,15 @@
+package fc5.toy3.board.domain.grade.type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum GradeType {
+
+    NEWBIE("새싹회원"),
+    VIP("우수회원")
+    ;
+
+    private final String description;
+}
