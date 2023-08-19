d555a20e965538039779bde0d13f3e5bdb7e7325
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-19 17:53:50 +0900

    feat: 회원 테이블 정의 (#3)
    - 한 회원이 여러 권한을 가질 수 있으므로 회원의 권한은 별도의 테이블로 정의 예정

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,3 +1,4 @@
+drop table if exists member cascade;
 drop table if exists authority cascade;
 drop table if exists grade cascade;
 
@@ -13,4 +14,16 @@ create table grade (
     name        varchar(16)     not null unique,
     created_at  timestamp       not null default now(),
     updated_at  timestamp
+);
+
+create table member (
+    id          bigint          not null auto_increment primary key,
+    grade_id    bigint          not null,
+    username    varchar(32)     not null unique,
+    email       varchar(64)     not null unique,
+    password    char(60)        not null,
+    nickname    varchar(16)     not null unique,
+    created_at  timestamp       not null default now(),
+    updated_at  timestamp,
+    foreign key (grade_id) references grade(id)
 );
