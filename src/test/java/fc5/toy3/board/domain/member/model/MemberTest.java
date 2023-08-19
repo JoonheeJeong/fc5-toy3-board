d794c9fc93df28ca434d2d202f4276aacbeaccaf
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-19 23:22:32 +0900

    feat(test): 무권한 회원에게 권한 하나 추가 (#3)

---
+++ src/test/java/fc5/toy3/board/domain/member/model/MemberTest.java
@@ -0,0 +1,50 @@
+package fc5.toy3.board.domain.member.model;
+
+import fc5.toy3.board.domain.authority.model.Authority;
+import fc5.toy3.board.domain.authority.repository.AuthorityRepository;
+import fc5.toy3.board.domain.authority.type.AuthorityType;
+import fc5.toy3.board.domain.grade.repository.GradeRepository;
+import fc5.toy3.board.domain.grade.type.GradeType;
+import fc5.toy3.board.domain.member.MemberFactory;
+import fc5.toy3.board.domain.member.MemberTestConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Import(MemberTestConfig.class)
+@SpringBootTest
+class MemberTest {
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+    @Autowired
+    private MemberFactory memberFactory;
+
+    @Transactional
+    @DisplayName("무권환 회원에게 권한 하나 추가")
+    @Test
+    void whenAddAnAuthorityToMemberWithNoAuthority_thenGetsOne() {
+        Member newbie = memberFactory.createNewbie(1);
+        AuthorityType[] authorityTypes = AuthorityType.values();
+        for (int i = 1; i <= authorityTypes.length; ++i) {
+            // given
+            newbie.getAuthorities().clear();
+
+            // when
+            Authority userAuthority = authorityRepository.findById((long) i);
+            newbie.addAuthority(userAuthority);
+
+            // then
+            assertThat(newbie.getAuthorities()).hasSize(1);
+            assertThat(newbie.getAuthorities().get(0)).isSameAs(userAuthority);
+        }
+    }
+}
