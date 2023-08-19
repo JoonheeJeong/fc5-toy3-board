9eedda98628e30b2f685af1c3e74abd9c3fe57ea
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-19 23:19:49 +0900

    feat: 회원-권한 다대다 필드 추가 (#3)
    - 회원-권한 생성 메소드 구현

--- src/main/java/fc5/toy3/board/domain/authority/model/Authority.java
+++ src/main/java/fc5/toy3/board/domain/authority/model/Authority.java
@@ -2,9 +2,13 @@ package fc5.toy3.board.domain.authority.model;
 
 import fc5.toy3.board.domain.BaseEntity;
 import fc5.toy3.board.domain.authority.type.AuthorityType;
+import fc5.toy3.board.domain.member.model.Member;
 import lombok.Getter;
+import lombok.NonNull;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
@@ -13,4 +17,14 @@ public class Authority extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @Column(name = "name", unique = true, nullable = false)
     private AuthorityType type;
+
+    @ManyToMany(fetch = FetchType.EAGER, mappedBy = "authorities")
+    private List<Member> members = new ArrayList<>();
+
+    public void addMember(@NonNull Member member) {
+        if (!members.contains(member)) {
+            members.add(member);
+            member.addAuthority(this);
+        }
+    }
 }
