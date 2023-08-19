3e340c7df261534f79d7a6b91af6f98b1765f264
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-19 16:51:33 +0900

    feat(test): 권한 dao 단건 조회 테스트 (#1)
    - 성공, 실패 케이스 간단하게 테스트

---
+++ src/test/java/fc5/toy3/board/domain/authority/repository/AuthorityRepositoryTest.java
@@ -0,0 +1,37 @@
+package fc5.toy3.board.domain.authority.repository;
+
+import fc5.toy3.board.domain.authority.exception.NotFoundAuthorityException;
+import fc5.toy3.board.domain.authority.model.Authority;
+import fc5.toy3.board.domain.authority.type.AuthorityType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+class AuthorityRepositoryTest {
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
+    @DisplayName("id 조회 성공")
+    @Test
+    void whenExists_thenGetsMember() {
+        AuthorityType[] values = AuthorityType.values();
+        for (int i = 1; i <= values.length; ++i) {
+            Authority foundAuthority = authorityRepository.findById((long) i);
+            assertThat(foundAuthority.getType())
+                    .isSameAs(values[i-1]);
+        }
+    }
+
+    @DisplayName("id 조회 실패")
+    @Test
+    void whenNotExists_thenThrows() {
+        assertThatThrownBy(() -> authorityRepository.findById(3L))
+                .isInstanceOf(NotFoundAuthorityException.class);
+    }
+}
