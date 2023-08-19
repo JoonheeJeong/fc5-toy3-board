5bf6a3cf699fb868ad0d1324c3948c665742ae81
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-19 16:54:34 +0900

    feat(test): 등급 dao 단건 조회 테스트 (#1)
    - 간단한 성공 및 실패 케이스 테스트

---
+++ src/test/java/fc5/toy3/board/domain/grade/repository/GradeRepositoryTest.java
@@ -0,0 +1,37 @@
+package fc5.toy3.board.domain.grade.repository;
+
+import fc5.toy3.board.domain.grade.exception.NotFoundGradeException;
+import fc5.toy3.board.domain.grade.model.Grade;
+import fc5.toy3.board.domain.grade.type.GradeType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+class GradeRepositoryTest {
+
+    @Autowired
+    private GradeRepository gradeRepository;
+
+    @DisplayName("id 조회 성공")
+    @Test
+    void whenExists_thenGetsMember() {
+        GradeType[] values = GradeType.values();
+        for (int i = 1; i <= values.length; ++i) {
+            Grade foundGrade = gradeRepository.findById((long) i);
+            assertThat(foundGrade.getType())
+                    .isSameAs(values[i-1]);
+        }
+    }
+
+    @DisplayName("id 조회 실패")
+    @Test
+    void whenNotExists_thenThrows() {
+        assertThatThrownBy(() -> gradeRepository.findById(3L))
+                .isInstanceOf(NotFoundGradeException.class);
+    }
+}
