a16b28e1a8204c7ce272400e7b7ff4e5e012e25f
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-19 16:47:58 +0900

    feat: 기본 엔티티 정의 (#1)
    - 모든 테이블이 자동생성 주-대리키인 id와 생성 및 수정 시각에 대한 컬럼을 갖는다고 간주합니다.

---
+++ src/main/java/fc5/toy3/board/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package fc5.toy3.board.domain;
+
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
