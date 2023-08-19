1a534dce11325d603e76d366502dc2dd0d46ddd9
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-19 16:52:08 +0900

    feat: 등급 모델 정의 (#1)

---
+++ src/main/java/fc5/toy3/board/domain/grade/model/Grade.java
@@ -0,0 +1,19 @@
+package fc5.toy3.board.domain.grade.model;
+
+import fc5.toy3.board.domain.BaseEntity;
+import fc5.toy3.board.domain.grade.type.GradeType;
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Getter
+@Entity
+public class Grade extends BaseEntity {
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name", unique = true, nullable = false)
+    private GradeType type;
+}
