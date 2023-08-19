3186005b95fac253c252d60230a519de3c9dd752
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-19 16:48:28 +0900

    feat: 권한 상수 enum 정의 (#1)

---
+++ src/main/java/fc5/toy3/board/domain/authority/type/AuthorityType.java
@@ -0,0 +1,15 @@
+package fc5.toy3.board.domain.authority.type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthorityType {
+
+    USER("사용자"),
+    ADMIN("관리자")
+    ;
+
+    private final String description;
+}
