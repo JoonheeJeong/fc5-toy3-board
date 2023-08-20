4967a4e27cf8df442da556a5440ae2384a773062
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-20 23:27:00 +0900

    feat: 인증 매니저 빈 설정 (#5)
    - 디비에서 회원 조회하여 인증 수행

--- src/main/java/fc5/toy3/board/global/security/SecurityConfig.java
+++ src/main/java/fc5/toy3/board/global/security/SecurityConfig.java
@@ -3,6 +3,9 @@ package fc5.toy3.board.global.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,5 +38,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        return new ProviderManager(daoAuthenticationProvider);
     }
 }
