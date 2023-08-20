c84b689e6d6d4d82c72fd21f8128d3660e31885a
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-21 01:23:30 +0900

    refactor: GET /board 불필요 코드 제거 (#5)

--- src/main/java/fc5/toy3/board/domain/board/controller/BoardController.java
+++ src/main/java/fc5/toy3/board/domain/board/controller/BoardController.java
@@ -1,22 +1,19 @@
 package fc5.toy3.board.domain.board.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
-
+@Slf4j
 @Controller
 public class BoardController {
 
     @GetMapping("/board")
-    public String getBoardPage(Authentication auth, HttpSession session, Model model) {
-        SecurityContext securityContext = (SecurityContext) session.getAttribute("SPRING_SECURITY_CONTEXT");
-        Authentication authentication = securityContext.getAuthentication();
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+    public String getBoardPage(Authentication auth, Model model) {
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
         String username = userDetails.getUsername();
         model.addAttribute("username", username);
         return "board";
