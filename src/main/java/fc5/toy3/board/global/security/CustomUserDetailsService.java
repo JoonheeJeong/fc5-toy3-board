24c50d94975d1e223b2052742080d6bf318f6ac1
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-20 23:25:51 +0900

    feat: UserDetailsService 구현체 작성 (#5)

---
+++ src/main/java/fc5/toy3/board/global/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package fc5.toy3.board.global.security;
+
+import fc5.toy3.board.domain.member.model.Member;
+import fc5.toy3.board.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username);
+        return new CustomUserDetails(member);
+    }
+}
