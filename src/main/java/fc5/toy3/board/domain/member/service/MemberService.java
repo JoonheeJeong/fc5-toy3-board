73cb8cf2e97c31ca948b9b4c57bef7b0bf4a0442
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-21 22:11:57 +0900

    refactor: `MemberService` 인터페이스 정의 (#5)
    - UserDetailsService 상속

---
+++ src/main/java/fc5/toy3/board/domain/member/service/MemberService.java
@@ -0,0 +1,6 @@
+package fc5.toy3.board.domain.member.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface MemberService extends UserDetailsService {
+}
