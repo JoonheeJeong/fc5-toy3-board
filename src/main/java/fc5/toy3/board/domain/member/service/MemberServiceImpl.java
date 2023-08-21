d60dca4a01b3cb2a3e7333284bf58e5e95b01115
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-21 22:14:22 +0900

    refactor: `CustomUserDetailsService` 삭제 후 MeberService 구현체 작성 (#5)

--- src/main/java/fc5/toy3/board/domain/member/service/MemberServiceImpl.java
+++ src/main/java/fc5/toy3/board/domain/member/service/MemberServiceImpl.java
@@ -1,22 +1,19 @@
-package fc5.toy3.board.global.security;
+package fc5.toy3.board.domain.member.service;
 
-import fc5.toy3.board.domain.member.model.Member;
 import fc5.toy3.board.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberRepository.findByUsername(username);
-        return new CustomUserDetails(member);
+        return memberRepository.findByUsername(username);
     }
 }
