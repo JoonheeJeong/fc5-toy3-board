dd0d72554a3b9d16630da9ac1b97ddd8dd8cfaed
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-22 23:32:21 +0900

    chore: 권한 클래스에 빌더 등록 (#5)
    - 권한별 회원 목록 다대다 맵핑 필드 final로 변경

--- src/main/java/fc5/toy3/board/domain/member/entity/Authority.java
+++ src/main/java/fc5/toy3/board/domain/member/entity/Authority.java
@@ -2,8 +2,7 @@ package fc5.toy3.board.domain.member.entity;
 
 import fc5.toy3.board.domain.BaseEntity;
 import fc5.toy3.board.domain.member.type.AuthorityType;
-import lombok.Getter;
-import lombok.NonNull;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
@@ -11,6 +10,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 @Entity
 public class Authority extends BaseEntity implements GrantedAuthority {
 
@@ -19,7 +21,7 @@ public class Authority extends BaseEntity implements GrantedAuthority {
     private AuthorityType type;
 
     @ManyToMany(fetch = FetchType.EAGER, mappedBy = "authorities")
-    private List<Member> members = new ArrayList<>();
+    private final List<Member> members = new ArrayList<>();
 
     @Override
     public String getAuthority() {
