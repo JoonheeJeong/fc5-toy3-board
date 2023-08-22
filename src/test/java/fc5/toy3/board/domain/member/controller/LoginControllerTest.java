69a442dc62e7c0baf8a88f20c1e28d1c19686400
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-22 23:32:43 +0900

    test: 인증된 회원 게시판 접속 허용 (#5)
    - WithMockCustomUser 애너테이션 정의
    - WithSecurityContextFactory 구현체 작성

--- src/test/java/fc5/toy3/board/domain/member/controller/LoginControllerTest.java
+++ src/test/java/fc5/toy3/board/domain/member/controller/LoginControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static fc5.toy3.board.domain.member.controller.WithMockCustomUser.USER_USERNAME;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -32,5 +32,16 @@ class LoginControllerTest {
         mockMvc.perform(get("/board"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrlPattern("**/login"));
+    }
+
+    @WithMockCustomUser
+    @DisplayName("인증된 회원 게시판 접속 허용")
+    @Test
+    void whenAuthenticated_thenSuccessToAccessBoard() throws Exception {
+        mockMvc.perform(get("/board"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("board"))
+                .andExpect(model().attributeExists("username"))
+                .andExpect(model().attribute("username", USER_USERNAME));
     }
 }
