01f74a8198ae64ae91072abecb6c45106207be0c
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-22 23:39:07 +0900

    refactor: `@AuthenticationPrincipal`로 바인딩 (#5)
    - 불필요한 로깅 애너테이션 삭제

--- src/main/java/fc5/toy3/board/domain/board/controller/BoardController.java
+++ src/main/java/fc5/toy3/board/domain/board/controller/BoardController.java
@@ -1,21 +1,18 @@
 package fc5.toy3.board.domain.board.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Slf4j
 @Controller
 public class BoardController {
 
     @GetMapping("/board")
-    public String getBoardPage(Authentication auth, Model model) {
-        UserDetails userDetails = (UserDetails) auth.getPrincipal();
-        String username = userDetails.getUsername();
-        model.addAttribute("username", username);
+    public String getBoardPage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        model.addAttribute("username", userDetails.getUsername());
         return "board";
     }
 }
