a74c1b0646519a1a125213e3682d5d31623782e7
Joonhee Jeong <jeonggoo75@gmail.com>
2023-08-23 20:14:04 +0900

    feat: 로그인 실패 시 에러메시지 포함 (#5)

--- src/main/java/fc5/toy3/board/domain/member/controller/LoginController.java
+++ src/main/java/fc5/toy3/board/domain/member/controller/LoginController.java
@@ -1,17 +1,23 @@
 package fc5.toy3.board.domain.member.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
-@Slf4j
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String getLoginPage() {
+    public String getLoginPage(
+            @RequestParam(required = false) String error,
+            Model model) {
+
+        if (error != null) {
+            model.addAttribute("error", error);
+        }
         return "login";
     }
 }
